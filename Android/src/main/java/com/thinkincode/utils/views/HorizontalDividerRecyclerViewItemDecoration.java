fb0adaa8d9fe3d543fdfc47d28fdb49b4de9cfac
Adil Hussain <adil@thinkincode.co.uk>
2016-12-03 17:15:55 +0000

    Updated *HorizontalDividerRecyclerViewItemDecoration* constructor to take in left and right margin parameter.

--- Android/src/main/java/com/thinkincode/utils/views/HorizontalDividerRecyclerViewItemDecoration.java
+++ Android/src/main/java/com/thinkincode/utils/views/HorizontalDividerRecyclerViewItemDecoration.java
@@ -7,7 +7,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
 /**
- * <p>An extension of {@link android.support.v7.widget.RecyclerView.ItemDecoration} that
+ * <p>An extension of {@link RecyclerView.ItemDecoration} that
  * – when applied to a {@link RecyclerView} –
  * draws a horizontal divider at the bottom of each item in the {@link RecyclerView}.</p>
  *
@@ -18,30 +18,39 @@ public class HorizontalDividerRecyclerViewItemDecoration extends RecyclerView.It
 
     private final Drawable dividerDrawable;
     private final int dividerHeightPixels;
+    private final int dividerMarginLeftPixels;
+    private final int dividerMarginRightPixels;
 
     /**
      * <p>Constructor.</p>
      *
      * @param dividerColor the color value to apply to the divider.
-     * @param dividerHeightPixels the height to apply to the divider.
+     * @param dividerHeightPixels the height to apply to the divider (in pixels).
+     * @param dividerMarginLeftPixels the size of the left margin (in pixels).
+     * @param dividerMarginRightPixels the size of the right margin (in pixels).
      */
-    public HorizontalDividerRecyclerViewItemDecoration(int dividerColor, int dividerHeightPixels) {
+    public HorizontalDividerRecyclerViewItemDecoration(int dividerColor,
+                                                       int dividerHeightPixels,
+                                                       int dividerMarginLeftPixels,
+                                                       int dividerMarginRightPixels) {
         this.dividerDrawable = new ColorDrawable(dividerColor);
         this.dividerHeightPixels = dividerHeightPixels;
+        this.dividerMarginLeftPixels = dividerMarginLeftPixels;
+        this.dividerMarginRightPixels = dividerMarginRightPixels;
     }
 
     @Override
     public void onDrawOver(Canvas c, RecyclerView parent, RecyclerView.State state) {
-        int left = parent.getPaddingLeft();
-        int right = parent.getWidth() - parent.getPaddingRight();
+        int left = dividerMarginLeftPixels;
+        int right = parent.getWidth() - dividerMarginRightPixels;
 
         int childCount = parent.getChildCount();
         for (int i = 0; i < childCount; i++) {
             View child = parent.getChildAt(i);
 
-            RecyclerView.LayoutParams params = (RecyclerView.LayoutParams) child.getLayoutParams();
+            RecyclerView.LayoutParams childLayoutParams = (RecyclerView.LayoutParams) child.getLayoutParams();
 
-            int top = child.getBottom() + params.bottomMargin;
+            int top = child.getBottom() + childLayoutParams.bottomMargin;
             int bottom = top + dividerHeightPixels;
 
             dividerDrawable.setBounds(left, top, right, bottom);
