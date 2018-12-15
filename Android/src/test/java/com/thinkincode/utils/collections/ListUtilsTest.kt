c2bbcdd26bc26bfeac1f210206026d672f8a9d85
Adil Hussain <adil.hussain@masabi.com>
2018-12-15 17:20:16 +0000

    Added *List<String>.joinWithSpaceSeparator(...)*, *CharSequence.splitOnSpaces()* and *CharSequence.numberOfCharactersExcludingArabicVowelMarks()* Kotlin extension functions.

---
+++ Android/src/test/java/com/thinkincode/utils/collections/ListUtilsTest.kt
@@ -0,0 +1,61 @@
+package com.thinkincode.utils.collections
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+class ListUtilsTest {
+
+    @Test
+    fun joinWithSpaceSeparator_without_to_index() {
+        // When.
+        val result = listOf("apple", "banana", "carrot").joinWithSpaceSeparator()
+
+        // Then.
+        assertEquals("apple banana carrot", result)
+    }
+
+    @Test
+    fun joinWithSpaceSeparator_with_to_index_of_0() {
+        // When.
+        val result = listOf("apple", "banana", "carrot").joinWithSpaceSeparator(0)
+
+        // Then.
+        assertEquals("", result)
+    }
+
+    @Test
+    fun joinWithSpaceSeparator_with_to_index_of_1() {
+        // When.
+        val result = listOf("apple", "banana", "carrot").joinWithSpaceSeparator(1)
+
+        // Then.
+        assertEquals("apple", result)
+    }
+
+    @Test
+    fun joinWithSpaceSeparator_with_to_index_of_2() {
+        // When.
+        val result = listOf("apple", "banana", "carrot").joinWithSpaceSeparator(2)
+
+        // Then.
+        assertEquals("apple banana", result)
+    }
+
+    @Test
+    fun joinWithSpaceSeparator_with_to_index_of_3() {
+        // When.
+        val result = listOf("apple", "banana", "carrot").joinWithSpaceSeparator(3)
+
+        // Then.
+        assertEquals("apple banana carrot", result)
+    }
+
+    @Test(expected = IndexOutOfBoundsException::class)
+    fun joinWithSpaceSeparator_with_to_index_of_4() {
+        // When.
+        listOf("apple", "banana", "carrot").joinWithSpaceSeparator(4)
+    }
+}
---
+++ Android/src/test/java/com/thinkincode/utils/lang/CharSequenceUtilsTest.kt
@@ -0,0 +1,109 @@
+package com.thinkincode.utils.lang
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+class CharSequenceUtilsTest {
+
+    @Test
+    fun splitOnSpaces_with_empty_string() {
+        // When.
+        val result = "".splitOnSpaces()
+
+        // Then.
+        assertEquals(emptyList<String>(), result)
+    }
+
+    @Test
+    fun splitOnSpaces_with_single_space() {
+        // When.
+        val result = " ".splitOnSpaces()
+
+        // Then.
+        assertEquals(emptyList<String>(), result)
+    }
+
+    @Test
+    fun splitOnSpaces_with_double_space() {
+        // When.
+        val result = "  ".splitOnSpaces()
+
+        // Then.
+        assertEquals(emptyList<String>(), result)
+    }
+
+    @Test
+    fun splitOnSpaces_with_three_words_and_single_spaces() {
+        // When.
+        val result = "apple banana carrot".splitOnSpaces()
+
+        // Then.
+        assertEquals(listOf("apple", "banana", "carrot"), result)
+    }
+
+    @Test
+    fun splitOnSpaces_with_three_words_and_double_spaces() {
+        // When.
+        val result = "apple  banana  carrot".splitOnSpaces()
+
+        // Then.
+        assertEquals(listOf("apple", "banana", "carrot"), result)
+    }
+
+    @Test
+    fun numberOfCharactersExcludingArabicVowelMarks_with_surah_1_ayah_1() {
+        // When.
+        val result = "بِسْمِ اللَّهِ الرَّحْمَٰنِ الرَّحِيمِ".numberOfCharactersExcludingArabicVowelMarks()
+
+        // Then.
+        assertEquals(22, result)
+    }
+
+    @Test
+    fun numberOfCharactersExcludingArabicVowelMarks_with_surah_2_ayah_1() {
+        // When.
+        val result = "الم".numberOfCharactersExcludingArabicVowelMarks()
+
+        // Then.
+        assertEquals(3, result)
+    }
+
+    @Test
+    fun numberOfCharactersExcludingArabicVowelMarks_with_surah_2_ayah_2() {
+        // When.
+        val result = "ذَٰلِكَ الْكِتَابُ لَا رَيْبَ ۛ فِيهِ ۛ هُدًى لِلْمُتَّقِينَ".numberOfCharactersExcludingArabicVowelMarks()
+
+        // Then.
+        assertEquals(37, result)
+    }
+
+    @Test
+    fun numberOfCharactersExcludingArabicVowelMarks_with_surah_2_ayah_4() {
+        // When.
+        val result = "وَالَّذِينَ يُؤْمِنُونَ بِمَا أُنْزِلَ إِلَيْكَ وَمَا أُنْزِلَ مِنْ قَبْلِكَ وَبِالْآخِرَةِ هُمْ يُوقِنُونَ".numberOfCharactersExcludingArabicVowelMarks()
+
+        // Then.
+        assertEquals(63, result)
+    }
+
+    @Test
+    fun numberOfCharactersExcludingArabicVowelMarks_with_surah_2_ayah_5() {
+        // When.
+        val result = "أُولَٰئِكَ عَلَىٰ هُدًى مِنْ رَبِّهِمْ ۖ وَأُولَٰئِكَ هُمُ الْمُفْلِحُونَ".numberOfCharactersExcludingArabicVowelMarks()
+
+        // Then.
+        assertEquals(42, result)
+    }
+
+    @Test
+    fun numberOfCharactersExcludingArabicVowelMarks_with_surah_4_ayah_148() {
+        // When.
+        val result = "لَا يُحِبُّ اللَّهُ الْجَهْرَ بِالسُّوءِ مِنَ الْقَوْلِ إِلَّا مَنْ ظُلِمَ ۚ وَكَانَ اللَّهُ سَمِيعًا عَلِيمًا".numberOfCharactersExcludingArabicVowelMarks()
+
+        // Then.
+        assertEquals(68, result)
+    }
+}
