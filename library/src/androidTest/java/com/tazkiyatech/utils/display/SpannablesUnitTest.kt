ec6401a71564e06e839264b1d103c65725592273
Adil Hussain <adil.hussain@masabi.com>
2019-03-22 17:51:45 +0000

    Added `SpannablesUnitTest`.

---
+++ library/src/androidTest/java/com/tazkiyatech/utils/display/SpannablesUnitTest.kt
@@ -0,0 +1,46 @@
+package com.tazkiyatech.utils.display
+
+import android.graphics.Color
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
+import android.text.style.StyleSpan
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class SpannablesUnitTest {
+
+    @Test
+    fun applyBoldStyleSpanToSubString() {
+        // Given.
+        val spannable = SpannableString("apple banana carrot")
+
+        // When.
+        spannable.applyBoldStyleSpanToSubString("banana")
+
+        // Then.
+        val spans = spannable.getSpans(0, spannable.length, StyleSpan::class.java)
+
+        assertEquals(1, spans.size)
+        assertEquals(6, spannable.getSpanStart(spans[0])) // 13
+        assertEquals(12, spannable.getSpanEnd(spans[0])) // 19
+    }
+
+    @Test
+    fun applyForegroundColorSpanToSubString() {
+        // Given.
+        val spannable = SpannableString("apple banana carrot")
+
+        // When.
+        spannable.applyForegroundColorSpanToSubString("banana", Color.BLACK)
+
+        // Then.
+        val spans = spannable.getSpans(0, spannable.length, ForegroundColorSpan::class.java)
+
+        assertEquals(1, spans.size)
+        assertEquals(6, spannable.getSpanStart(spans[0])) // 13
+        assertEquals(12, spannable.getSpanEnd(spans[0])) // 19
+    }
+}
