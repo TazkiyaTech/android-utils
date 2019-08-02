67745643afbef1060015e4fb4d96d4c66162d65e
Adil Hussain <adil.hussain@masabi.com>
2019-08-02 22:17:17 +0100

    Changed the signature of the `DialogFragment.showAllowingStateLoss(fragmentManager: FragmentManager, tag: String)` function to `DialogFragment.showAllowingStateLoss(fragmentManager: FragmentManager, tag: String?)`.

--- library/src/main/java/com/tazkiyatech/utils/display/DialogFragmentExtensions.kt
+++ library/src/main/java/com/tazkiyatech/utils/display/DialogFragmentExtensions.kt
@@ -6,6 +6,6 @@ import androidx.fragment.app.FragmentManager
 /**
  * Like [DialogFragment.show] but allows the show to be executed after an activity's state is saved.
  */
-fun DialogFragment.showAllowingStateLoss(fragmentManager: FragmentManager, tag: String) {
+fun DialogFragment.showAllowingStateLoss(fragmentManager: FragmentManager, tag: String?) {
     fragmentManager.beginTransaction().add(this, tag).commitAllowingStateLoss()
 }
