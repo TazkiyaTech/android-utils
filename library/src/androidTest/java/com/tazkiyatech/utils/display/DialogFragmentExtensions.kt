463df17a8d52c1aeb1da38c6cb11b92d4f76f8ad
Adil Hussain <adil.hussain@masabi.com>
2019-08-02 19:50:45 +0100

    Added `DialogFragment.showAllowingStateLoss(...)` Kotlin extension function.

---
+++ library/src/androidTest/java/com/tazkiyatech/utils/display/DialogFragmentExtensions.kt
@@ -0,0 +1,11 @@
+package com.tazkiyatech.utils.display
+
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.FragmentManager
+
+/**
+ * Like [DialogFragment.show] but allows the show to be executed after an activity's state is saved.
+ */
+fun DialogFragment.showAllowingStateLoss(fragmentManager: FragmentManager, tag: String) {
+    fragmentManager.beginTransaction().add(this, tag).commitAllowingStateLoss()
+}
