bf963be9679cde451eee6ebe6643e84278111536
Adil Hussain <adillson05uk@gmail.com>
2024-03-15 11:57:37 +0000

    Converted the `build.gradle` file in the `library` module to `build.gradle.kts` and raised the `minSdk` value from 19 to 23.

---
+++ library/build.gradle.kts
@@ -0,0 +1,122 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    `maven-publish`
+    signing
+}
+
+android {
+    compileSdk = 34
+    namespace = "com.tazkiyatech.utils"
+
+    defaultConfig {
+        minSdk = 23
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        debug {
+            isMinifyEnabled = false
+            isShrinkResources = false
+        }
+        release {
+            isMinifyEnabled = false
+            isShrinkResources = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+
+    lint {
+        abortOnError = true
+    }
+
+    publishing {
+        singleVariant("release") {
+            withJavadocJar()
+            withSourcesJar()
+        }
+    }
+}
+
+dependencies {
+    api(libs.appcompat)
+    api(libs.recyclerview)
+    implementation(libs.annotation)
+
+    testImplementation(libs.hamcrest.core)
+    testImplementation(libs.junit.jupiter.api)
+    testImplementation(libs.junit.jupiter.engine)
+    testImplementation(libs.junit.jupiter.params)
+
+    androidTestImplementation(libs.test.ext.junit)
+    androidTestImplementation(libs.test.runner)
+    androidTestImplementation(libs.test.rules)
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("release") {
+            afterEvaluate {
+                from(components["release"])
+            }
+
+            groupId = project.properties["groupId"].toString()
+            artifactId = project.properties["artifactId"].toString()
+            version = project.properties["version"].toString()
+
+            pom {
+                name = project.properties["artifactId"].toString()
+                description =
+                    "An Android library containing a mixture of small helper classes, functions and views useful for Android projects."
+                url = "https://github.com/TazkiyaTech/android-utils"
+                licenses {
+                    license {
+                        name = "The Apache License, Version 2.0"
+                        url = "https://www.apache.org/licenses/LICENSE-2.0.txt"
+                    }
+                }
+                organization {
+                    name = "Tazkiya Tech"
+                    url = "http://tazkiyatech.com"
+                }
+                developers {
+                    developer {
+                        id = "adil-hussain-84"
+                        name = "Adil Hussain"
+                        email = "adilson05uk@gmail.com"
+                    }
+                }
+                scm {
+                    connection = "scm:git:git://github.com/TazkiyaTech/android-utils.git"
+                    developerConnection = "scm:git:ssh://github.com:TazkiyaTech/android-utils.git"
+                    url = "https://github.com/TazkiyaTech/android-utils"
+                }
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            name = "sonatype"
+            credentials(PasswordCredentials::class)
+            url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+        }
+    }
+}
+
+signing {
+    // the "signing.keyId", "signing.password" and "signing.secretKeyRingFile" properties required by this task are defined outside of this project in the "~/.gradle/gradle.properties" file
+    sign(publishing.publications["release"])
+}
